1402ca6025425d412bdf1a8baa5d11de956879b6
GraTec <741445849@qq.com>
2020-12-12 03:29:25 -0800

    Added biasVSDweight

---
+++ biasSVDweight.py
@@ -0,0 +1,69 @@
+import numpy as np
+import time
+from math import sqrt
+
+
+def computeSSE(m, p, q, lamb, b_user, b_item, sigma):
+    [m_row, m_col, m_val] = m[0:3]
+    length = len(m_row)
+    SSE = 0
+    for i in range(0, length):
+        SSE += (m_val[i]-np.dot(p[m_row[i], :], q[:, m_col[i]])-sigma-b_user[m_row[i]]-b_item[m_col[i]]) ** 2+lamb * \
+            (np.linalg.norm(p[m_row[i], :])**2 + np.linalg.norm(q[:, m_col[i]])
+             ** 2+np.linalg.norm(b_user)**2+np.linalg.norm(b_item)**2)
+    return SSE
+
+
+def biasSVDweight(m, a, maxK, lamb):
+    print('Initializing...')
+    [m_row, m_col, m_val, row, col] = m
+    length = len(m_row)
+    [b_user, b_item, sigma] = [np.zeros(row), np.zeros(col), sum(m_val)/length]
+    step = 1
+    p = np.zeros((row, maxK))
+    q = np.zeros((maxK, col))
+    weight = np.zeros(row)
+    for i in range(length):
+        weight[m_row[i]] += 1
+    weight = weight / row
+    for k in range(0, maxK):
+        for i in m_row:
+            p[i][k] = np.random.rand()
+        for j in m_col:
+            q[k][j] = np.random.rand()
+    print('Computing first RMSE...')
+    SSE = computeSSE(m, p, q, lamb, b_user, b_item, sigma)
+    RMSE = sqrt(SSE/length)
+    print('Initialization finished. RMSE=', RMSE, '. Factoring matrix...')
+    while step <= 10**5:
+        print('Running step', step, 'RMSE=', RMSE, end='\r')
+        # time.sleep(0.5)
+        b2_user = b_user
+        b2_item = b_item
+        for i in range(0, length):
+            user = m_row[i]
+            item = m_col[i]
+            _sum = m_val[i]-np.dot(p[user, :], q[:, item]) - \
+                sigma-b_user[user]-b_item[item]
+            for k in range(0, maxK):
+                tmp = p[user][k]*weight[user]  # Add weight here
+                p[user][k] = p[user][k]+a * \
+                    (_sum * q[k][item]-lamb*p[user][k])/RMSE
+                q[k][item] = q[k][item]+a*(_sum*tmp-lamb*q[k][item])/RMSE
+        b_user[user] = b_user[user]+a * \
+            (_sum*length-lamb*b_user[user])/RMSE
+        b_item[item] = b_item[item]+a * \
+            (_sum*length-lamb*b_item[item])/RMSE
+        # for i in m_row:
+        #     p[i][k] = p[i][k]+2*a*_sum*q[k][item]
+        # for j in m_col:
+        #     q[k][j] = q[k][j]+2*a*_sum*tmp
+        newSSE = computeSSE(m, p, q, lamb, b_user, b_item, sigma)
+        newRMSE = sqrt(newSSE/length)
+        if abs(newRMSE-RMSE) <= 10**-5:
+            print('', end='\n')
+            return [p, q, sigma, b_user, b_item]
+        step += 1
+        RMSE = newRMSE
+    print('', end='\n')
+    return [p, q, sigma, b_user, b_item]
--- finals.py
+++ finals.py
@@ -3,6 +3,7 @@ from SGD import SGD
 from loadData import loadData
 from nSGD import nSGD
 from biasSVD import biasSVD
+from biasSVDweight import biasSVDweight
 from math import sqrt
 import random
 from dataSplit import dataSplit, getTrainTest
@@ -48,20 +49,66 @@ def finalTest():
     # Reading finished
     # Split data
     dimMax = 10**3
-    m2 = dataSplit(m, [dimMax, dimMax])
-    [m2_row, m2_col, m2_val] = m2
+    m1 = dataSplit(m, [dimMax, dimMax])
+    [m1_train, m1_test] = getTrainTest(m1)
+    m1 = dataSplit(m, [2*dimMax, 2*dimMax])
+    [m2_train, m2_test] = getTrainTest(m1)
+    m1 = dataSplit(m, [3*dimMax, 3*dimMax])
+    [m3_train, m3_test] = getTrainTest(m1)
     # Split finished
-    [m_train, m_test] = getTrainTest(m2)
-    [p, q] = SGD(m_train, 0.001, 10)
-    print(computeRMSE(m_test, p, q))
+    [p1, q1] = SGD(m1_train, 0.001, 10)
+    [p2, q2] = SGD(m2_train, 0.001, 10)
+    [p3, q3] = SGD(m3_train, 0.001, 10)
+    RMSE_SGD = [0, 0, 0]
+    RMSE_SGD[0] = computeRMSE(m1_test, p1, q1)
+    RMSE_SGD[1] = computeRMSE(m2_test, p2, q2)
+    RMSE_SGD[2] = computeRMSE(m3_test, p3, q3)
+    print(RMSE_SGD)
     # print(p)
 
-    [p, q] = nSGD(m_train, 0.005, 10, 0.1)
-    print(computeRMSE(m_test, p, q))
+    [p1, q1] = nSGD(m1_train, 0.005, 10, 0.1)
+    [p2, q2] = nSGD(m2_train, 0.005, 10, 0.1)
+    [p3, q3] = nSGD(m3_train, 0.005, 10, 0.1)
+    RMSE_nSGD = [0, 0, 0]
+    RMSE_nSGD[0] = computeRMSE(m1_test, p1, q1)
+    RMSE_nSGD[1] = computeRMSE(m2_test, p2, q2)
+    RMSE_nSGD[2] = computeRMSE(m3_test, p3, q3)
+    print(RMSE_nSGD)
     # print(p)
 
-    [p, q, sigma, b_user, b_item] = biasSVD(m_train, 0.0025, 10, 0.5)
-    print(computeRMSEbias(m_test, p, q, sigma, b_user, b_item))
+    [p1, q1, sigma1, b_user1, b_item1] = biasSVD(m1_train, 0.0025, 10, 0.5)
+    [p2, q2, sigma2, b_user2, b_item2] = biasSVD(m2_train, 0.0001, 10, 0.5)
+    [p3, q3, sigma3, b_user3, b_item3] = biasSVD(m3_train, 0.0001, 10, 0.5)
+    RMSE_biasSVD = [0, 0, 0]
+    RMSE_biasSVD[0] = computeRMSEbias(
+        m1_test, p1, q1, sigma1, b_user1, b_item1)
+    RMSE_biasSVD[1] = computeRMSEbias(
+        m2_test, p2, q2, sigma2, b_user2, b_item2)
+    RMSE_biasSVD[2] = computeRMSEbias(
+        m3_test, p3, q3, sigma3, b_user3, b_item3)
+    print(RMSE_biasSVD)
+
+    [p1, q1, sigma1, b_user1, b_item1] = biasSVDweight(
+        m1_train, 0.0025, 10, 0.5)
+    [p2, q2, sigma2, b_user2, b_item2] = biasSVDweight(
+        m2_train, 0.0001, 10, 0.5)
+    [p3, q3, sigma3, b_user3, b_item3] = biasSVDweight(
+        m3_train, 0.0001, 10, 0.5)
+    RMSE_biasSVDweight = [0, 0, 0]
+    RMSE_biasSVDweight[0] = computeRMSEbias(
+        m1_test, p1, q1, sigma1, b_user1, b_item1)
+    RMSE_biasSVDweight[1] = computeRMSEbias(
+        m2_test, p2, q2, sigma2, b_user2, b_item2)
+    RMSE_biasSVDweight[2] = computeRMSEbias(
+        m3_test, p3, q3, sigma3, b_user3, b_item3)
+    print(RMSE_biasSVDweight)
+
+    with open('./result', 'w') as f:
+        f.write(str(RMSE_SGD))
+        f.write(str(RMSE_nSGD))
+        f.write(str(RMSE_biasSVD))
+        f.write(str(RMSE_biasSVDweight))
+        f.close()
 
 
 # test()
